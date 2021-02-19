2a3be4040d803aa1d8dc6db36d0a7335656acdc2
jamiemalcolm <jmalcolm281@gmail.com>
2021-02-19 12:50:22 +0000

    drunkit has sellfor and type

--- src/main/java/items/DrumKit.java
+++ src/main/java/items/DrumKit.java
@@ -3,15 +3,36 @@ package items;
 import behaviour.IPlay;
 import behaviour.ISell;
 
-public class DrumKit implements IPlay, ISell {
+public class DrumKit extends Instrument{
+    private int numOfDrums;
+    private int numOfSymbols;
+    private InstrumentType instrumentType;
+
+    public DrumKit(String sound, double boughtFor, boolean isAcoustic, int numOfDrums, int numOfSymbols) {
+        super(sound, boughtFor, isAcoustic);
+        this.numOfDrums = numOfDrums;
+        this.numOfSymbols = numOfSymbols;
+        this.instrumentType = InstrumentType.PERCUSSION;
+    }
 
 
+    public int getNumOfDrums() {
+        return numOfDrums;
+    }
+
+    public int getNumOfSymbols() {
+        return numOfSymbols;
+    }
 
     public String play() {
-        return null;
+        return this.getSound();
     }
 
     public double calcMarkUp() {
-        return 0;
+        return this.getSellFor();
+    }
+
+    public InstrumentType getInstrumentType() {
+        return instrumentType;
     }
 }
---
+++ src/test/java/DrumKitTest.java
@@ -0,0 +1,30 @@
+import items.DrumKit;
+import items.InstrumentType;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DrumKitTest {
+
+    DrumKit drumKit;
+
+
+    @Before
+    public void before(){
+        drumKit = new DrumKit("boom boom ba boom claaang", 300.00, true, 4, 3);
+    }
+
+
+
+    @Test
+    public void hasType() {
+        assertEquals(InstrumentType.PERCUSSION, drumKit.getInstrumentType());
+    }
+
+    @Test
+    public void hasSellFor(){
+        assertEquals(450.00, drumKit.calcMarkUp(), 0.01);
+    }
+}
